57ab1fa3c7814a70b76d3bf3b9fa3b8e3311255b
muqshots <muqshots1777@gmail.com>
2022-02-25 22:04:35 +0400

    Fix stream url

--- apps/web-app/src/pages/tv/[id]/[key].tsx
+++ apps/web-app/src/pages/tv/[id]/[key].tsx
@@ -28,9 +28,9 @@ const StreamTV = ({
 }: InferNextProps<typeof getServerSideProps>) => {
   const getStreamUrl = (server: string = defaultServer) => {
     return isProd
-      ? process.env.NEXT_PUBLIC_STREAM_BASE
-        ? process.env.NEXT_PUBLIC_STREAM_BASE
-        : `https://${server.toLowerCase()}.movies4discord.xyz` +
+      ? (process.env.NEXT_PUBLIC_STREAM_BASE
+          ? process.env.NEXT_PUBLIC_STREAM_BASE
+          : `https://${server.toLowerCase()}.movies4discord.xyz`) +
           `?viewkey=${viewKey}`
       : "https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4";
   };
