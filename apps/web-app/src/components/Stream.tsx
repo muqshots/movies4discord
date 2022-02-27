71cf6e92e7cf192badd3e6c163f92c027c1c13f9
muqshots <muqshots1777@gmail.com>
2022-02-27 12:38:48 +0400

    Fix resuming

--- apps/web-app/src/components/Stream.tsx
+++ apps/web-app/src/components/Stream.tsx
@@ -55,45 +55,55 @@ export const Stream = ({
   useEffect(() => {
     let active = true;
 
+    async function handleLoad(e: Plyr.PlyrEvent) {
+      if (!active) return;
+      const percentage = (
+        await ky
+          .get(`/api/history`, {
+            searchParams: {
+              ...historyParams,
+              one: true,
+            },
+          })
+          .json<{ percentage: number | null }>()
+      ).percentage;
+      if (percentage) {
+        e.detail.plyr.currentTime = (percentage / 100) * e.detail.plyr.duration;
+      }
+    }
+
+    const handleProgress = throttle(5000, async (e: Plyr.PlyrEvent) => {
+      if (active && e.detail.plyr.currentTime > 0) {
+        await ky.post("/api/history", {
+          searchParams: {
+            ...historyParams,
+            percentage:
+              (e.detail.plyr.currentTime / e.detail.plyr.duration) * 100,
+          },
+        });
+      }
+    });
+
     const addListeners = () => {
       if (!ref.current) return;
       const plyr = ref.current.plyr;
 
-      plyr.on("loadedmetadata", async () => {
-        if (!active) return;
-        const percentage = (
-          await ky
-            .get(`/api/history`, {
-              searchParams: {
-                ...historyParams,
-                one: true,
-              },
-            })
-            .json<{ percentage: number | null }>()
-        ).percentage;
-        if (percentage) {
-          plyr.currentTime = (percentage / 100) * plyr.duration;
-        }
-      });
+      plyr.on("loadedmetadata", handleLoad);
+      plyr.on("progress", handleProgress);
+    };
 
-      plyr.on(
-        "progress",
-        throttle(5000, () => {
-          if (active && plyr.currentTime > 0)
-            ky.post("/api/history", {
-              searchParams: {
-                ...historyParams,
-                percentage: (plyr.currentTime / plyr.duration) * 100,
-              },
-            });
-        })
-      );
+    const removeListeners = () => {
+      if (!ref.current) return;
+      const plyr = ref.current.plyr;
+      plyr.off("loadedmetadata", handleLoad);
+      plyr.off("progress", handleProgress);
     };
 
     setTimeout(addListeners, 0);
 
     return () => {
       active = false;
+      removeListeners();
     };
   });
 
