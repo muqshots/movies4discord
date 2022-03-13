df31c77d8715ae144e08c1f46c6d7ae612012aed
muqshots <muqshots1777@gmail.com>
2022-03-13 12:23:26 +0400

    use functional component

--- apps/web-app/src/pages/_document.tsx
+++ apps/web-app/src/pages/_document.tsx
@@ -1,33 +1,20 @@
-import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=optional"
+        />
+      </Head>
+      <body className="bg-theme text-white scrollbar-hide">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=optional"
-          />
-        </Head>
-        <body className="bg-theme scrollbar-hide text-white">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-export default MyDocument;
+export default Document;
