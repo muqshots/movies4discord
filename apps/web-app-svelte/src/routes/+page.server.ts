e9797635b954391bab162bd44e8eb9b211f0d0d6
Razorback360 <82079079+Razorback360@users.noreply.github.com>
2024-04-06 07:07:08 +0300

    sample prisma request

---
+++ apps/web-app-svelte/src/routes/+page.server.ts
@@ -0,0 +1,12 @@
+// src/routes/+page.server.ts
+
+import {prisma} from "@movies4discord/db"
+import type { PageServerLoad } from './$types';
+
+export const load = (async () => {
+// 1.
+const response = await prisma.introHash.findMany({})
+
+// 2.
+return { feed: response };
+}) satisfies PageServerLoad;
